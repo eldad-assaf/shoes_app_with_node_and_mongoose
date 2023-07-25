ee0a1e1e4101fd4dbce6d5ee722a64b48e9a7c09
eldad-assaf <eldadux@gmail.com>
2023-07-25 19:37:24 +0300

    first commit

---
+++ lib/views/ui/mainscreen.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+import 'package:provider/provider.dart';
+import 'package:shoes_app_with_node_and_mongoose/views/ui/profile.dart';
+import 'package:shoes_app_with_node_and_mongoose/views/ui/searchpage.dart';
+
+import '../../controllers/mainscreen_provider.dart';
+import '../shared/bottom_nav.dart';
+import 'cartpage.dart';
+import 'homepage.dart';
+
+// ignore: must_be_immutable
+class MainScreen extends StatelessWidget {
+  MainScreen({super.key});
+
+  List<Widget> pageList = [
+    const HomePage(),
+    const SearchPage(),
+    const HomePage(),
+    CartPage(),
+    const ProfilePage()
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Consumer<MainScreenNotifier>(
+      builder: (context, mainScreenNotifier, child) {
+        return Scaffold(
+          backgroundColor: const Color(0xFFE2E2E2),
+          body: pageList[mainScreenNotifier.pageIndex],
+          bottomNavigationBar: const BottoNavBar(),
+        );
+      },
+    );
+  }
+}
